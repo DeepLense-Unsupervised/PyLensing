7f0540b2d963c626cfdf2c192b0f78652aaa0700
Pranath Reddy <f20160572@hyderabad.bits-pilani.ac.in>
2020-06-24 20:59:06 +0530

    update setup script

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo PyAutoLens installation - Docker
 git clone https://github.com/Jammy2211/autolens_workspace
+cd autolens_workspace/
+git checkout 5acb22d22539ccac3328d65d64e29c66aaa24fc7
+cd ..
 export WORKSPACE=./autolens_workspace/
 export PYTHONPATH=./autolens_workspace
 
