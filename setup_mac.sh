a33574ddba970bcbf865f92186a3daeef3d90d09
Pranath Reddy <f20160572@hyderabad.bits-pilani.ac.in>
2020-06-24 20:33:12 +0530

    Update setup script

--- setup_mac.sh
+++ setup_mac.sh
@@ -6,10 +6,13 @@ export PATH="/usr/local/anaconda3/bin:$PATH"
 conda create -n autolens python=3.7 anaconda
 conda activate autolens
 conda install -c conda-forge multinest
-pip install autolens
-pip3 install autolens
+pip install autolens==0.46.2
+pip3 install autolens==0.46.2
 # Change the below lines to your working directory
 git clone https://github.com/Jammy2211/autolens_workspace
+cd autolens_workspace/
+git checkout 5acb22d22539ccac3328d65d64e29c66aaa24fc7
+cd ..
 export WORKSPACE=/Users/pranath/autolens_workspace
 export PYTHONPATH=/Users/pranath/autolens_workspace
 echo Setup is Complete
