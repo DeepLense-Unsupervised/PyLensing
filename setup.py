a0a59a49ec3081d6ce448bdb0a9ce0ece700ff22
Pranath Reddy <f20160572@hyderabad.bits-pilani.ac.in>
2020-08-19 23:44:20 +0530

    Added setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'lensing',
+  packages = ['PyLensing'],
+  version = '0.1',
+  license='MIT',
+  description = 'A tool for generating lensing images based on PyAutoLens simulations',
+  author = 'K Pranath Reddy',
+  author_email = 'pranath.mail@gmail.com',
+  url = 'https://github.com/DeepLense-Unsupervised/PyLensing',
+  keywords = ['Gravitational Lensing', 'Simulation', 'Dark Matter'],
+  install_requires=[            
+          'numpy==1.18.3',
+          'scipy==1.4.1',
+          'joblib==0.14.1',
+          'Cython==0.29.16',
+          'pandas==1.0.3',
+          'progress==1.5',
+          'pymultinest==2.9',
+          'autolens==0.46.2',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+  ],
+)
